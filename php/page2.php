04c2c4aba1bdabd26450dc014fb9555b8f276a56
Yao Li <43403868+YaoLi93@users.noreply.github.com>
2019-09-26 14:49:52 -0500

    Add files via upload

---
+++ php/page2.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+//echo "username: ".$_GET["username"].", password: ".$_GET["password"];
+echo "username: ".$_GET["username"].", password: ".$_GET["password"];
+//$_SESSION["usr"] = $_GET["username"];
+?>
+
+
+<!--
+/*
+session_start();
+
+echo "get: ". $_GET["username"]."\n"
+echo "session: ". $_SESSION["usr"];
+*/
+
+
+?>-->
